67ffa42f580cc7e4155a8eb9a9cb3097aa704544
Mauro Jose Lista Marin <126710594+Mauricitoxx@users.noreply.github.com>
2025-06-19 16:15:03 -0300

    Update endpointsdb.py

--- backend/endpointsdb.py
+++ backend/endpointsdb.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # o ["http://localhost:3000"]
+    allow_origins=[
+        "http://localhost:3000",
+        "https://tp-comunicacion.vercel.app/"
+    ],  # o ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
